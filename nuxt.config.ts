b83411ab1a3ae92706423e0167a3f0331288c585
wakin <liwakin@qq.com>
2025-07-24 11:52:00 +0800

    chore: remove gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/wakin-blog/',
+  },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/image', '@nuxtjs/tailwindcss'],
